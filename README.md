cc7c15a8eb8bcdf8c79ea1cda0d8e3d97a48c601
cartewane <cartewane@gmail.com>
2025-07-19 19:28:54 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bokepditaman
-bokepditaman
+
+[bokepditaman](https://bokepditaman.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Bokep Ditaman';
+export const desk = 'Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
+export const url = 'https://bokepditaman.pages.dev';
+export const terbit = '2025-07-19T01:01:03Z';
+
+export const google = 'c4UjlNf9Pgo0epKRg37mB7kcdbzDa2sDgSDWAafQhe0';
+export const yandex = '29eaed5d626a322b'
+
+export const index = '8b7ea0c8-0b08-45cc-8cf7-58319d140e7c';
